93cd7c262aa3ad042bd22290e552b883cc368961
vadim11041996 <vadim11041996@hotmail.com>
2018-05-13 22:59:44 +0300

    asd

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');	
+var app = express();	
+var bodyParser = require('body-parser');	
+var mongoose = require('mongoose');	
+mongoose.connect('mongodb://localhost/blog2018');	
+	
+var PostSchema = mongoose.Schema({	
+  //title: {type: String, required: true},	
+  body:String,	
+  posted:{type: Date, default:Date.now}	
+});	
+	
+app.use(express.static(__dirname + '/public'));	
+app.use(bodyParser.json());	
+app.use(bodyParser.urlencoded({extended:true}));	
+	
+app.post("/api/blogpost", createPost);	
+	
+function createPost(req, res){	
+    var post = req.body;	
+    console.log(post);	
+    res.json(post);	
+}	
+	
+app.listen(3005);
